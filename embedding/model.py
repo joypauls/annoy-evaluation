197a9b850d0fc383505c6ccd05146dc8688599bd
joypauls <joypaulsen3@gmail.com>
2022-04-20 01:58:40 -0500

    building script to generate embeddings dataset

---
+++ embedding/model.py
@@ -0,0 +1,62 @@
+# using Tensorflow's tutorial https://www.tensorflow.org/recommenders/examples/basic_retrieval
+import numpy as np
+import tensorflow as tf
+
+user_model = tf.keras.Sequential([
+  tf.keras.layers.StringLookup(
+      vocabulary=unique_user_ids, mask_token=None),
+  # We add an additional embedding to account for unknown tokens.
+  tf.keras.layers.Embedding(len(unique_user_ids) + 1, embedding_dimension)
+])
+
+class NoBaseClassMovielensModel(tf.keras.Model):
+
+  def __init__(self, user_model, movie_model):
+    super().__init__()
+    self.movie_model: tf.keras.Model = movie_model
+    self.user_model: tf.keras.Model = user_model
+    self.task: tf.keras.layers.Layer = task
+
+  def train_step(self, features: Dict[Text, tf.Tensor]) -> tf.Tensor:
+
+    # Set up a gradient tape to record gradients.
+    with tf.GradientTape() as tape:
+
+      # Loss computation.
+      user_embeddings = self.user_model(features["user_id"])
+      positive_movie_embeddings = self.movie_model(features["movie_title"])
+      loss = self.task(user_embeddings, positive_movie_embeddings)
+
+      # Handle regularization losses as well.
+      regularization_loss = sum(self.losses)
+
+      total_loss = loss + regularization_loss
+
+    gradients = tape.gradient(total_loss, self.trainable_variables)
+    self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
+
+    metrics = {metric.name: metric.result() for metric in self.metrics}
+    metrics["loss"] = loss
+    metrics["regularization_loss"] = regularization_loss
+    metrics["total_loss"] = total_loss
+
+    return metrics
+
+  def test_step(self, features: Dict[Text, tf.Tensor]) -> tf.Tensor:
+
+    # Loss computation.
+    user_embeddings = self.user_model(features["user_id"])
+    positive_movie_embeddings = self.movie_model(features["movie_title"])
+    loss = self.task(user_embeddings, positive_movie_embeddings)
+
+    # Handle regularization losses as well.
+    regularization_loss = sum(self.losses)
+
+    total_loss = loss + regularization_loss
+
+    metrics = {metric.name: metric.result() for metric in self.metrics}
+    metrics["loss"] = loss
+    metrics["regularization_loss"] = regularization_loss
+    metrics["total_loss"] = total_loss
+
+    return metrics
